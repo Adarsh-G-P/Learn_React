81689f8d65cbf2e36397fc64bd659228e75da4c9
reactpgm <adarshgp96@gmail.com>
2023-08-17 17:22:57 +0530

    displaying the details multiple times

--- src/App.js
+++ src/App.js
@@ -5,12 +5,22 @@ function App() {
   const name = <h1>Alex</h1>
   const age = <h1>25</h1>
   const email = <h1>alex25@gmail.com</h1>
-
-  return (
-    <div className="App">
+  const user =(
+    <div>
       {name}
       {age}
       {email}
+
+    </div>
+
+  )
+  return (
+    <div className="App">
+      {user}
+      {user}
+      {user}
+      {user}
+
       </div>
   );
 }
