1650705160811b49b4e2fd11200c9b722d823afe
reactpgm <adarshgp96@gmail.com>
2023-08-17 17:15:30 +0530

    added name age email addded

--- src/App.js
+++ src/App.js
@@ -2,17 +2,15 @@
 import './App.css';
 
 function App() {
-  const name = <h1>"Alex"</h1>
+  const name = <h1>Alex</h1>
+  const age = <h1>25</h1>
+  const email = <h1>alex25@gmail.com</h1>
+
   return (
     <div className="App">
       {name}
-      {name}
-      {name}
-      {name}
-
-
-
-
+      {age}
+      {email}
       </div>
   );
 }
