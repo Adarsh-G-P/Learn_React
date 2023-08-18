a5e342c3bbe4c3a29a0ab913d363eac4d3a85a83
reactpgm <adarshgp96@gmail.com>
2023-08-18 12:19:18 +0530

    added some css to page

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ function App() {
     <Job salary={90000} position="Senior SDE" company="Amazon"/>
     <Job salary={25000} position="Junior SDE" company="Google"/>
     <Job salary={50000} position="Project Manager" company="Netflix"/>
+    <h1 className="name"> ABC LIMITED</h1>
     </div>
+    
   );
 }
 
@@ -15,7 +17,7 @@ function App() {
 const Job = (props) => {
   return (
   <div>
-    <h1>{props.salary}</h1>
+    <h1 className='cash'>{props.salary}</h1>
     <h2>{props.position}</h2>
     <h3>{props.company}</h3>
   
