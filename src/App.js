b7dae81a5d066f8f70c24d6181592f7d88eaacf1
reactpgm <adarshgp96@gmail.com>
2023-08-18 13:38:36 +0530

    ternary operator ?:, &&

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import './App.css';
 
 function App() {
   const age =5
+  const isGreen = true
     return (
     <div className="App">
-      {age>=18 ? <h1>Over age</h1> : <h1>Under age</h1>}
+      {age >=18 ? <h1>Over age</h1> : <h1>Under age</h1>}
+      <h1 style={{color: isGreen ? "green" : "orange"}}>This display color</h1>
+      {isGreen && <button>Green-Button</button>}
       </div>
     );
  
