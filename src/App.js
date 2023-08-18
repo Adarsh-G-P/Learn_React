419fc8b61a153ddafb716ab9fd64dd9decb233fe
reactpgm <adarshgp96@gmail.com>
2023-08-18 13:04:43 +0530

    conditional rendering using if..else

--- src/App.js
+++ src/App.js
@@ -2,28 +2,15 @@
 import './App.css';
 
 function App() {
- return (
-    <div className="App">
-    <Job salary={90000} position="Senior SDE" company="Amazon"/>
-    <Job salary={25000} position="Junior SDE" company="Google"/>
-    <Job salary={50000} position="Project Manager" company="Netflix"/>
-    <h1 className="name"> ABC LIMITED</h1>
-    </div>
-    
-  );
+  const age = 22
+  if(age>=18){
+    return <h1>OVER AGE</h1>;
+  }
+  else{
+    return <div className="App"><h1>UNDER AGE</h1></div>;
+  }
+ 
+  
 }
-
-
-const Job = (props) => {
-  return (
-  <div>
-    <h1 className='cash'>{props.salary}</h1>
-    <h2>{props.position}</h2>
-    <h3>{props.company}</h3>
-  
-  
-  </div>
-  );
-};
 
 export default App;
