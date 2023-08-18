c7b071242316d6ffc79a1b351b5d3212c6b7d0c1
reactpgm <adarshgp96@gmail.com>
2023-08-18 11:44:15 +0530

    multiple user details using props

--- src/App.js
+++ src/App.js
@@ -4,19 +4,21 @@ import './App.css';
 function App() {
  return (
     <div className="App">
-     <User />
+     <User name = "ALex" age={25} email = "alex@gmail.com" />
+     <User name = "Aliya" age={27} email = "aliya@gmail.com" />
+     <User name = "Fariz" age={35} email = "fariz@gmail.com" />
    
     </div>
   );
 }
 
 
-const User = () => {
+const User = (props) => {
   return (
   <div>
-   <h1>Alex</h1>
-   <h1>25</h1>
-   <h1>alex@gmail.com</h1>
+   <h1>{props.name}</h1>
+   <h1>{props.age}</h1>
+   <h1>{props.email}</h1>
   
   </div>
   );
