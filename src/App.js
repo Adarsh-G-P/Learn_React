65b3959807edce03a6955151d302d54b23b3702d
reactpgm <adarshgp96@gmail.com>
2023-08-18 16:32:17 +0530

    print multiple list elements using map()

--- src/App.js
+++ src/App.js
@@ -2,15 +2,25 @@
 import './App.css';
 
 function App() {
-  const names =["Alex","Aliya","Omer","Fiza","Peter"];
-    return (
+  const users =[
+    {name:"Alex", age:26},
+    {name: "Adam",age:28},
+    {name:"Fida",age: 43},
+  ];
+
+
+  return (
     <div className="App">
-      {names.map((name) => {
-        return <h1 > {name}</h1>
-      })}
+    {users.map((user,key) => {
+    return <div> 
+      <h1>name={user.name}</h1>
+      <h1>age={user.age}</h1>  
       </div>
-    );
- 
+    })}
+    </div>
+  );
+
+
    
 }
 
