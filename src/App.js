a95cc99d8fcf84951c3ffa30de58f617f57a0877
reactpgm <adarshgp96@gmail.com>
2023-08-23 14:32:00 +0530

    sample pgm print elements using map()

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ const planets = [{name : "Mars", isGasplanet: false},
   {name : "Jupiter", isGasplanet: true},
   {name : "Venus", isGasplanet: false},
   {name : "Neptune", isGasplanet: true},
-  {name : "Uranus", isGasplanet: true},
+  {name : " Uranus", isGasplanet: true},
 ];
 
 return (
   <div className="App">
-    { planets.map((planet,key) => {
-      if (planet.isGasplanet) return <h1>{planet.name}</h1>;
+    { planets.map(
+      (planet,key) => !planet.isGasplanet && <h1>{planet.name}</h1>
       
-    })}
+    )}
     
   </div>
   );
